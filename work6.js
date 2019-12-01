cc7dae7413c364b2069332720febae5a8d432f24
judybs <57298099+judybs@users.noreply.github.com>
2019-12-01 10:29:46 +0800

    Add files via upload

---
+++ work6.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <script type="text/javascript">
+        function push(array, ...items) {
+    array.push(...items);
+    }
+
+    function add(x,x ) {
+    return x*x;
+    }
+
+    const numbers = [-4,-1,0,3,10];
+    add(...numbers) 
+    </script>
+    
+</body>
+</html>
